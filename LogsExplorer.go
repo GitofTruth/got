51a19ac28358b70dae9410c2ee1dcfe45b9f4d34
mickey-me <mfaisal@aucegypt.edu>
2019-10-10 22:49:24 +0200

    commiting to use the desktop

--- LogsExplorer.go
+++ LogsExplorer.go
@@ -1,1 +1,27 @@
 package GoT
+
+import (
+	"gopkg.in/src-d/go-billy.v4/memfs"
+	"gopkg.in/src-d/go-git.v4"
+)
+
+type LogsExplorer struct {
+	Repo *git.Repository
+}
+
+
+func CreateNewLogsExplorer(path string) LogsExplorer, err{
+
+}
+
+
+func (le *LogsExplorer) OpenRepo(path string) (bool, error) {
+	fs := memfs.New()
+	repo, err := git.PlainOpenWithOptions(path, fs)
+	if err != nil {
+		panic(err)
+	} else {
+		le.Repo = repo
+		return true, nil
+	}
+}
---
+++ LogsExplorer_test.go
@@ -0,0 +1,10 @@
+package GoT
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLogsExplorer(t *testing.T) {
+	fmt.Println("Starting Test \t\t Main")
+}
