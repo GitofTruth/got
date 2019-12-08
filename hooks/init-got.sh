b58f4a2aa154e70d0c2ae68c120918eb619d9bc4
Muhammad Faisal <mfaisal@aucegypt.edu>
2019-12-08 08:55:45 +0200

    finished preparing hooks for thesis one demo (bug chance 70%)

--- hooks/init-got.sh
+++ hooks/init-got.sh
@@ -1,4 +1,8 @@
 git --bare init .got
 git remote set-url origin .got
-cp $GOPATH/src/github.com/GitofTruth/GoT/hooks/pre-push .git/hooks
-cp $GOPATH/src/github.com/GitofTruth/GoT/hooks/pre-push .got/hooks
+#cp $GOPATH/src/github.com/GitofTruth/GoT/hooks/repo/pre-push .git/hooks
+cp $GOPATH/src/github.com/GitofTruth/GoT/hooks/bare/post-receive .got/hooks
+cp $GOPATH/src/github.com/GitofTruth/GoT/hooks/bare/pre-receive .got/hooks
+mkdir .gotconfig
+git push
+git pull
