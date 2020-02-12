84881d5372aa755ee13dcff3e7a364b0c05e9797
Muhammad Faisal <mfaisal@aucegypt.edu>
2020-02-12 18:30:31 +0200

    Adding skeleton for new database scheme

--- remote/RepoContract.go
+++ remote/RepoContract.go
@@ -8,71 +8,98 @@ import (
 	"github.com/GitofTruth/GoT/datastructures"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
-	sc "github.com/hyperledger/fabric/protos/peer"
+	"github.com/hyperledger/fabric/protos/peer"
 )
 
 type RepoContract struct {
 }
 
 //how to pass variables for initialization?
-func (contract *RepoContract) Init(APIstub shim.ChaincodeStubInterface) sc.Response {
+func (contract *RepoContract) Init(stub shim.ChaincodeStubInterface) peer.Response {
+
 	fmt.Println("initializing ledger")
-	pushNumber := 0
-	pushNumberBytes, _ := json.Marshal(pushNumber)
-	APIstub.PutState("PushNumber", pushNumberBytes)
-	fmt.Println("Ledger initalized push number is ")
-	fmt.Println(pushNumberBytes)
+
+	// Add none repo?
+
+
+	// pushNumber := 0
+	// pushNumberBytes, _ := json.Marshal(pushNumber)
+	// stub.PutState("PushNumber", pushNumberBytes)
+	// fmt.Println("Ledger initalized push number is ")
+	// fmt.Println(pushNumberBytes)
+
 	return shim.Success(nil)
 }
 
-func (contract *RepoContract) getCurrentRepoState(APIstub shim.ChaincodeStubInterface) (datastructures.Repo, int) {
+
+func (contract *RepoContract) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+
+	function, args := stub.GetFunctionAndParameters()
+
+	if function == "addNewRepo" {
+		return contract.addNewRepo(stub, args)
+	} else if function == "addNewBranch" {
+		return contract.addNewBranch(stub, args)
+	} else if function == "addCommits" {
+		return contract.addCommits(stub, args)
+	} else if function == "addCollabrator" {
+		return contract.addCollabrator(stub, args)
+	}
+
+	else if function == "queryRepo" {
+		return contract.queryRepo(stub, args)
+	} else if function == "queryBranch" {
+		return contract.queryBranch(stub, args)
+	} else if function == "queryRepoHashes" {
+		return contract.queryRepoHashes(stub, args)
+	} else if function == "queryCommits" {
+		return contract.queryCommits(stub, args)
+	} else if function == "queryCollabrators" {
+		return contract.queryCollabrators(stub, args)
+	}
+
+
+	return shim.Error("Invalid Smart Contract function name.")
+}
+
+func(contract *RepoContract) addNewRepo(stub shim.ChaincodeStubInterface) peer.Response {
+
+	
+}
+
+func (contract *RepoContract) getCurrentRepoState(stub shim.ChaincodeStubInterface) (datastructures.Repo, int) {
 	repo, _ := datastructures.CreateNewRepo("", 0, nil)
 	master, _ := datastructures.CreateNewRepoBranch("master", "client", 0, nil)
 	repo.AddBranch(master)
-	pushes := contract.getAllPushes(APIstub)
+	pushes := contract.getAllPushes(stub)
 
 	for _, push := range pushes {
 		repo.AddCommitLogs(push.Logs, push.BranchName)
 	}
 
-	pushNumberBytes, _ := APIstub.GetState("PushNumber")
+	pushNumberBytes, _ := stub.GetState("PushNumber")
 	var pushNumber int
 	json.Unmarshal(pushNumberBytes, &pushNumber)
 
 	return repo, pushNumber
 }
 
-func (contract *RepoContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response {
 
-	function, args := APIstub.GetFunctionAndParameters()
-
-	if function == "push" {
-		return contract.addPush(APIstub, args)
-	} else if function == "getBetween" {
-		return contract.getPushes(APIstub, args)
-	} else if function == "addBranch" {
-		return contract.addBranch(APIstub, args)
-	} else if function == "getBranches" {
-		return contract.getBranches(APIstub, args)
-	}
-
-	return shim.Error("Invalid Smart Contract function name.")
-}
 
 //pushAsBytes, _ := json.Marshal(args[1])
-//APIstub.PutState(args[0], pushAsBytes)
+//stub.PutState(args[0], pushAsBytes)
 
-// carAsBytes, _ := APIstub.GetState(args[0])
+// carAsBytes, _ := stub.GetState(args[0])
 // return shim.Success(carAsBytes)
 
 //need to generate hash as key instead of just the same object
-func (contract *RepoContract) addPush(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+func (contract *RepoContract) addPush(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 
 	if len(args) != 1 {
 		return shim.Error("Incorrect number of arguments. Expecting 1")
 	}
 
-	repo, pushNumber := contract.getCurrentRepoState(APIstub)
+	repo, pushNumber := contract.getCurrentRepoState(stub)
 
 	//it should be Marshaled on submission
 	//pushAsBytes, _ := json.Marshal(args[1]) //changes from json object to bytes (string)
@@ -83,11 +110,11 @@ func (contract *RepoContract) addPush(APIstub shim.ChaincodeStubInterface, args 
 
 	if done, _ := repo.AddCommitLogs(pushLog.Logs, pushLog.BranchName); done {
 		startKeyBytes, _ := json.Marshal(pushNumber)
-		APIstub.PutState(string(startKeyBytes), []byte(args[0]))
+		stub.PutState(string(startKeyBytes), []byte(args[0]))
 
 		pushNumber = pushNumber + 1
 		pushNumberBytes, _ := json.Marshal(pushNumber)
-		APIstub.PutState("PushNumber", pushNumberBytes)
+		stub.PutState("PushNumber", pushNumberBytes)
 
 		return shim.Success(nil)
 	}
@@ -95,13 +122,13 @@ func (contract *RepoContract) addPush(APIstub shim.ChaincodeStubInterface, args 
 	return shim.Error("Invalid push Log!")
 }
 
-func (contract *RepoContract) addBranch(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+func (contract *RepoContract) addBranch(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 
 	if len(args) != 1 {
 		return shim.Error("Incorrect number of arguments. Expecting 1")
 	}
 
-	repo, _ := contract.getCurrentRepoState(APIstub)
+	repo, _ := contract.getCurrentRepoState(stub)
 
 	var branch datastructures.RepoBranch
 	json.Unmarshal([]byte(args[0]), &branch)
@@ -110,7 +137,7 @@ func (contract *RepoContract) addBranch(APIstub shim.ChaincodeStubInterface, arg
 
 	if done, _ := repo.AddBranch(branch); done {
 		fmt.Println("New branch added:\t" + branch.Name)
-		APIstub.PutState(branch.Name, []byte(args[0]))
+		stub.PutState(branch.Name, []byte(args[0]))
 		return shim.Success(nil)
 	}
 
@@ -118,16 +145,16 @@ func (contract *RepoContract) addBranch(APIstub shim.ChaincodeStubInterface, arg
 }
 
 //
-// func (contract *RepoContract) initLedger(APIstub shim.ChaincodeStubInterface) sc.Response {
+// func (contract *RepoContract) initLedger(stub shim.ChaincodeStubInterface) peer.Response {
 // 	return shim.Success(nil)
 // }
 
-func (contract *RepoContract) getAllPushes(APIstub shim.ChaincodeStubInterface) []datastructures.PushLog {
+func (contract *RepoContract) getAllPushes(stub shim.ChaincodeStubInterface) []datastructures.PushLog {
 
 	startKeyBytes, _ := json.Marshal(0)
-	endKeyBytes, _ := APIstub.GetState("PushNumber")
+	endKeyBytes, _ := stub.GetState("PushNumber")
 
-	resultsIterator, err := APIstub.GetStateByRange(string(startKeyBytes), string(endKeyBytes))
+	resultsIterator, err := stub.GetStateByRange(string(startKeyBytes), string(endKeyBytes))
 	if err != nil {
 		return make([]datastructures.PushLog, 0)
 	}
@@ -150,7 +177,7 @@ func (contract *RepoContract) getAllPushes(APIstub shim.ChaincodeStubInterface) 
 	return pushlogs
 }
 
-func (contract *RepoContract) getPushes(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+func (contract *RepoContract) getPushes(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 
 	fmt.Println("Querying the ledger..")
 
@@ -160,7 +187,7 @@ func (contract *RepoContract) getPushes(APIstub shim.ChaincodeStubInterface, arg
 	startKeyBytes, _ := json.Marshal(startKey)
 	endKeyBytes, _ := json.Marshal(endKey)
 
-	resultsIterator, err := APIstub.GetStateByRange(string(startKeyBytes), string(endKeyBytes))
+	resultsIterator, err := stub.GetStateByRange(string(startKeyBytes), string(endKeyBytes))
 	if err != nil {
 		return shim.Error(err.Error())
 	}
@@ -190,9 +217,9 @@ func (contract *RepoContract) getPushes(APIstub shim.ChaincodeStubInterface, arg
 	return shim.Success(pushlogsjson)
 }
 
-func (contract *RepoContract) getBranches(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+func (contract *RepoContract) getBranches(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 	fmt.Println("Querying the ledger..")
-	repo, _ := contract.getCurrentRepoState(APIstub)
+	repo, _ := contract.getCurrentRepoState(stub)
 
 	brancgesjson, _ := json.Marshal(repo.GetBranches())
 
---
+++ remote/WriteData.go
@@ -0,0 +1,12 @@
+package main
+
+import(
+  "fmt"
+
+  "github.com/hyperledger/fabric/core/chaincode/shim"
+  "github.com/hyperledger/fabric/protos/peer"
+  "github.com/hyperledger/fabric/protos/ledger/queryresult"
+
+
+  "strconv"
+)
