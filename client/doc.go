0858c296f6c58f781dc4658d1937ec755812902e
Muhammad Faisal <mfaisal@aucegypt.edu>
2020-10-27 10:50:58 +0200

    Update doc.go

--- client/doc.go
+++ client/doc.go
@@ -1,2 +1,2 @@
-// This package has been created as a client for the got system. However, as hyperledger still not fully supporting golang. This package now implements only enough structures to ensure users management inlcuding user creation, modification, deletion and authentication.
+// This is a retired sub-package that was intended as a client for the got system. However, as hyperledger still not fully supporting golang. This package now implements only enough structures to ensure users management inlcuding user creation, modification, deletion and authentication.
 package client
